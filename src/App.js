85c6141adf0eb7c9213d972f3428d2ee399e9dad
koraykoksal <kksl.koray@gmail.com>
2023-07-29 11:49:44 +0300

    tailwindcss installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 
 function App() {
   return (
+    <>
+
     <div>
-      <h1>TailwindCss Kurulum</h1>
+    <h1 className="text-3xl 
+      font-bold 
+      underline 
+      text-center">TailwindCss Kurulum</h1>
     </div>
+
+
+    <div>
+      <h1 className=" bg-red-600 text-2xl text-center">Tailwind</h1>
+    </div>
+
+      <div>
+      <div className="bg-[#bebe] 
+          flex flex-col 
+          justify-between 
+          items-center 
+          rounded-xl 
+          bg-slate-500 h-[250px] w-3/12 m-10 text-center">
+            <h1>KORAY</h1>
+          </div>
+
+          <div className="bg-[#bebe] 
+          flex flex-col 
+          justify-between 
+          items-center 
+          rounded-xl 
+          bg-slate-500 h-[250px] w-3/12 m-10 text-center">
+            <h1>KORAY</h1>
+          </div>
+      </div>
+    
+      
+    </>
+
+    
   );
 }
 
