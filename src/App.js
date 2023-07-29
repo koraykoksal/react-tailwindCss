6f93cce7f402cc5aac0cbc6975f67e6e7af708aa
koraykoksal <kksl.koray@gmail.com>
2023-07-29 12:34:25 +0300

    tailwind components

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <h1 className=" bg-red-600 text-2xl text-center">Tailwind</h1>
     </div>
 
-      <div>
+      <div className="flex flex-col sm:flex-row gap-3 mt-3">
 
       <div className="m-10">
         <img src="https://pbs.twimg.com/profile_images/1417052605776375813/Jc9RL5o7_400x400.jpg"
@@ -25,6 +25,10 @@ function App() {
       <div className="card">
         <h1>KORAY</h1>
         <p className="line-clamp-1 hover:line-clamp-none p-3">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+      </div>
+
+      <div>
+        <button className="btn">Click Me</button>
       </div>
 
       </div>
